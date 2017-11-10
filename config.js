f8ae31dbae6ff6e1c0e8de11f75d61a9d544c045
Trigork <trigork@gmail.com>
2017-11-10 17:19:46 +0100

    Fixed RetweetCount Bug

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
-	'database': 'mongodb://localhost:27017/oshwtest',
+	'database': 'mongodb://localhost:27017/pre-oshwdem-tests',
+	// Change to production database
 	'secret': 'ilovescotchscotchyscotchscotch'
 };
