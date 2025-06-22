264b4242d23f9445de2fa402bdd26eee99ff836a
alexborrazasm <alexborrazasm@gmail.com>
2025-06-23 00:51:06 +0200

    Refactor package.json scripts and remove gulp-nodemon dependency
    - Removed "gulp-nodemon" from dependencies.
    - Added "build" and "dev" scripts for improved workflow.
    - Updated "start" script to use nodemon for server monitoring.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,8 @@
-// Load the plugins
-const { src, dest, task, series, watch } = require('gulp');
+const { src, dest, task, series } = require('gulp');
 const less = require('gulp-less');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
-const nodemon = require('gulp-nodemon');
 
-// Compile LESS to minified CSS
 function buildCSS() {
   return src('public/assets/css/style.less')
     .pipe(less())
@@ -14,30 +11,11 @@ function buildCSS() {
     .pipe(dest('public/assets/css'));
 }
 
-// Copy animate.css from node_modules
 function copyAnimateCSS() {
   return src('node_modules/animate.css/animate.min.css')
     .pipe(dest('public/assets/css'));
 }
 
-// Start server with Nodemon and watch for changes
-function startServer(cb) {
-  let started = false;
-
-  return nodemon({
-    script: 'server.js',
-    ext: 'js html less',
-    tasks: ['css', 'copy-css'],
-    env: { 'NODE_ENV': 'development' }
-  }).on('start', function () {
-    if (!started) {
-      cb();
-      started = true;
-    }
-  });
-}
-
-// Define tasks (Gulp 4/5 style)
 task('css', buildCSS);
 task('copy-css', copyAnimateCSS);
-task('start', series('css', 'copy-css', startServer));
+task('build', series('css', 'copy-css'));
