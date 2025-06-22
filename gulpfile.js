f95d677687c329140764714b3778cb8802cdd739
alexborrazasm <alexborrazasm@gmail.com>
2025-06-23 00:27:31 +0200

    Update gulp to version 5.0.1 in devDependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,43 @@
-// load the plugins
-var gulp      = require('gulp');
-var less      = require('gulp-less');
-var cleanCSS = require('gulp-clean-css');
-var nodemon = require('gulp-nodemon')
-var rename    = require('gulp-rename');
+// Load the plugins
+const { src, dest, task, series, watch } = require('gulp');
+const less = require('gulp-less');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const nodemon = require('gulp-nodemon');
 
-// define a task called css
-gulp.task('css', function() {
-	// grab the less file, process the LESS, save to style.css
-	return gulp.src('public/assets/css/style.less')
-		.pipe(less())
-		.pipe(cleanCSS())
-		.pipe(rename({ suffix: '.min' }))
-		.pipe(gulp.dest('public/assets/css'));
-});
+// Compile LESS to minified CSS
+function buildCSS() {
+  return src('public/assets/css/style.less')
+    .pipe(less())
+    .pipe(cleanCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(dest('public/assets/css'));
+}
 
-gulp.task('copy-css', function() {
-  return gulp.src('node_modules/animate.css/animate.min.css')
-    .pipe(gulp.dest('public/assets/css'));
-});
+// Copy animate.css from node_modules
+function copyAnimateCSS() {
+  return src('node_modules/animate.css/animate.min.css')
+    .pipe(dest('public/assets/css'));
+}
 
-gulp.task('start', function () {
-  nodemon({
-    script: 'server.js'
-  , ext: 'js html less'
-  , tasks: ['css', 'copy-css']
-  , env: { 'NODE_ENV': 'development' }
-  })
-});
+// Start server with Nodemon and watch for changes
+function startServer(cb) {
+  let started = false;
+
+  return nodemon({
+    script: 'server.js',
+    ext: 'js html less',
+    tasks: ['css', 'copy-css'],
+    env: { 'NODE_ENV': 'development' }
+  }).on('start', function () {
+    if (!started) {
+      cb();
+      started = true;
+    }
+  });
+}
+
+// Define tasks (Gulp 4/5 style)
+task('css', buildCSS);
+task('copy-css', copyAnimateCSS);
+task('start', series('css', 'copy-css', startServer));
